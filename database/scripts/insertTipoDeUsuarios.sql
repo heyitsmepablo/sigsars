48324c14a847f87b7373dbbbfeac5864f02f41a2
heyitsmepablo <pabloeduardossantos@gmail.com>
2025-06-08 00:06:24 -0300

    feat(database): implementado script sql  para cadastro de tipo de usuario

---
+++ database/scripts/insertTipoDeUsuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario_tipo (nome, descricao) 
+VALUES 
+  ('admin', 'Usuário com todos os privilégios do sistema'),
+  ('comum', 'Usuário com permissões básicas, sem acesso administrativo.');
